76198dfbc3bdac3217af85c210c55c6fbcfb75a9
RafaelFacundo <rafaelfacundo63@gmail.com>
2023-04-22 20:47:41 -0300

    react native proj init

---
+++ app/_layout.js
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+/* 
+    nós vamos usar a stack para linkar com todoas
+    as páginas do nosso app
+*/
+
+const Layout = () => {
+    return <Stack />
+};
+
+export default Layout;
